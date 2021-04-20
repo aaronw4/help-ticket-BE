0084f90d6158ae7a1facc13c8922fca5147517d7
Aaron Williams <aaronrw4@yahoo.com>
2021-04-19 23:01:03 -0700

    Created server.js

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const cors = require("cors");
+
+const userRoute = require("./users/users-route");
+const ticketRoute = require("./auth/tickets/tickets-route");
+const adminRoute = require("./auth/admin/admin-router");
+
+const server = express();
+
+server.use(express.json());
+server.use(cors());
+
+server.use("/api/user", userRoute);
+server.use("/api/ticket", ticketRoute);
+server.use("/api/admin", adminRoute);
+
+server.get("/", (req, res) => {
+  res.send("GET / Server base");
+});
+
+module.exports = server;
