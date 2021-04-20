c1ba65d6608d92f1de59e796742c658e268c6715
Aaron Williams <aaronrw4@yahoo.com>
2021-04-20 00:26:19 -0700

    Added production

--- knexfile.js
+++ knexfile.js
@@ -19,5 +19,21 @@ module.exports = {
         conn.run('PRAGMA foreign_keys = ON', done); 
       },
     }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
   }
 };
